ab03e372177291c5788c5b875f671d4646b76adb
francooo <72412439+francolovera24@users.noreply.github.com>
2025-06-19 12:45:08 -0300

    Delete TPODatos2 directory

--- TPODatos2/TPODatos2/src/main/java/connectors/RedisConnector.java
+++
@@ -1,9 +0,0 @@
-package connectors;
-
-import redis.clients.jedis.Jedis;
-
-public class RedisConnector {
-    public static Jedis conectar() {
-        return new Jedis("localhost", 6379);
-    }
-}
